f3a330448cdaa9100e5c1b7edb81439b37f19e06
Justin <jvcruz@ucdavis.edu>
2018-03-06 18:00:21 -0800

    fixes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
  
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
