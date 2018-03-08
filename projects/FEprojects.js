3b3255607eab3a6107f0e178bf1645ecca10d551
Justin <jvcruz@ucdavis.edu>
2018-03-08 04:24:39 -0800

    populated frontend projects

--- projects/FEprojects.js
+++ projects/FEprojects.js
@@ -1,2 +1,44 @@
 module.exports = [
+	{
+	  	name: 'Weather App',
+	    thumbnail: 'https://i.imgur.com/QzgNZfA.png',
+	  	liveLink: 'https://codepen.io/jvcruz/full/wqyXoL/',
+	    github: 'https://github.com/jcruzz/Weather-App',
+	    language: 'Javascript',
+	    stack: 'HTML, CSS',
+	    info: `
+							An application I created in August 2017 that uses the OpenWeatherMap 
+							Api to generate the Local Weather. The interface comes with a switch 
+							where you can switch between Celsius and Fahrenheit. Possibly due to 
+							the time I've created this, the image links to help generate the 
+							weather visual expired.
+	          `
+	},
+	{
+	  	name: 'NBA Quotes',
+	    thumbnail: 'https://i.imgur.com/gL6EC1y.png',
+	  	liveLink: 'https://codepen.io/jvcruz/full/prNYrd/',
+	    github: 'https://github.com/jcruzz/NBA-Quotes',
+	    language: 'Javascript',
+	    stack: 'HTML, CSS',
+	    info: `
+	            A Project I did a last year.  This front end project utilizes basic 
+	            Javascript in generating random quotes. The quotes generated can be 
+	            tweeted using a convenient button that grabs whatever generated quote 
+	            and opens up a new window.
+	          `
+	},
+	{
+	  	name: 'Scottie Pippen Tribute Page',
+	    thumbnail: 'https://i.imgur.com/uMj31sQ.png',
+	  	liveLink: 'https://codepen.io/jvcruz/full/YxGxGr/',
+	    github: 'https://github.com/jcruzz/SP-Tribute-Page',
+	    language: 'Javascript',
+	    stack: 'HTML, CSS',
+	    info: `
+	            My first completed page that started my journey in the world of 
+	            development. This is a simple tribute page for an overlooked legend in 
+	            Scottie Pippen.
+	          `
+	},
 ];
