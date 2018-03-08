0fcf2319bb3a704db5e7bd3c733ad794378b1401
Justin <jvcruz@ucdavis.edu>
2018-03-07 22:16:16 -0800

    created info, stack, and language properties

--- projects/projects.js
+++ projects/projects.js
@@ -3,30 +3,49 @@ module.exports = [
   	name: 'Polling.io',
     thumbnail: 'https://i.imgur.com/u1zwc3I.png',
     liveLink: 'http://polling-io.herokuapp.com/',
-  	github: 'https://github.com/jcruzz/Polling.io'
+  	github: 'https://github.com/jcruzz/Polling.io',
+    language: 'Javascript',
+    stack: 'MariaDB, React, React-Router, Redux, JSONWebToken, Redis, bcrypt, Chart.js',
+    info: `
+          I created this application with the intention for people to create polls in order to enhance their discussion.
+          In order to create polls, you must make an account since polls are bound to a unique user. However, as someone
+          without an account, you're free to vote in polls at your convenience.
+          `
   },
   {
   	name: 'Country Force Graph',
     thumbnail: 'https://i.imgur.com/FTtFOjm.png',
     liveLink: 'https://ancient-sierra-43985.herokuapp.com/',
-  	github: 'https://github.com/jcruzz/country-force-graph'
+  	github: 'https://github.com/jcruzz/country-force-graph',
+    language: null,
+    stack: null,
+    info: null
   },
   {
   	name: 'Just.ly',
     thumbnail: 'https://i.imgur.com/OKToW2a.png',
   	liveLink: 'https://just-ly.herokuapp.com/',
-    github: 'https://github.com/jcruzz/just.ly'
+    github: 'https://github.com/jcruzz/just.ly',
+    language: null,
+    stack: null,
+    info: null
   },
   {
     name: 'Global Heat Map',
     thumbnail: 'https://i.imgur.com/6aZiHY0.png',
     liveLink: 'https://codepen.io/jvcruz/full/jZoejG/',
-    github: 'https://github.com/jcruzz/global-heat-map'
+    github: 'https://github.com/jcruzz/global-heat-map',
+    language: null,
+    stack: null,
+    info: null
   },
   {
     name: 'JWT-Auth React Boilerplate',
     thumbnail: null,
     liveLink: 'https://frozen-depths-11875.herokuapp.com/',
-    github: 'https://github.com/jcruzz/react-router-auth-boilerplate-sql'
+    github: 'https://github.com/jcruzz/react-router-auth-boilerplate-sql',
+    language: null,
+    stack: null,
+    info: null
   },
 ];
