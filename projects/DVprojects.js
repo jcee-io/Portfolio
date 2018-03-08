47c3ab9de11bb24bbbf4e80f40341e845c496744
Justin <jvcruz@ucdavis.edu>
2018-03-08 02:14:25 -0800

    data visualization projects updated

--- projects/DVprojects.js
+++ projects/DVprojects.js
@@ -1,26 +1,52 @@
 module.exports = [
   {
-  	name: 'Country Force Graph',
+    name: 'Country Force Graph',
     thumbnail: 'https://i.imgur.com/FTtFOjm.png',
     liveLink: 'https://ancient-sierra-43985.herokuapp.com/',
-  	github: 'https://github.com/jcruzz/country-force-graph'
+    github: 'https://github.com/jcruzz/country-force-graph',
+    language: 'Javascript',
+    stack: 'D3.js, HTML, CSS',
+    info: `
+            This is a force graph created by connecting the countries with other countries located very closely
+            or are bordering each other. The nodes used for the graph are the flags of each respective countries,
+            which are linked with lines.
+          `
   },
   {
   	name: 'Biking Allegation Scatterplot',
     thumbnail: 'https://i.imgur.com/M1i3eVy.png',
     liveLink: 'https://codepen.io/jvcruz/full/oERGxa/',
-  	github: 'https://github.com/jcruzz/biking-allegation-scatterplot'
+  	github: 'https://github.com/jcruzz/biking-allegation-scatterplot',
+    language: 'Javascript',
+    stack: 'D3.js, HTML, CSS',
+    info: `
+            This is a scatterplot conducted on the 35 fastest times up Alpe d'Huez, and explaining the type of 
+            Doping Allegation if any drugs were used on the biker to enhance their performance.
+          `
   },
   {
-  	name: 'Global Heat Map',
+    name: 'Global Heat Map',
     thumbnail: 'https://i.imgur.com/6aZiHY0.png',
     liveLink: 'https://codepen.io/jvcruz/full/jZoejG/',
-  	github: 'https://github.com/jcruzz/global-heat-map'
+    github: 'https://github.com/jcruzz/global-heat-map',
+    language: 'Javascript',
+    stack: 'D3.js, HTML, CSS',
+    info: `
+            This is a heat map created to measure the land surface temperature from 1753 to 2015. 
+            The graph is populated by bars that correspends to the years and months respectively 
+            located at the X and Y axis, and using the color to determine the temperature. More
+            information can be shown by hoving over the bar.
+          `
   },
   {
   	name: 'GDP Bar Chart',
     thumbnail: 'https://i.imgur.com/hVGR4pe.png',
     liveLink: 'https://codepen.io/jvcruz/full/wybgKq/',
-  	github: 'https://github.com/jcruzz/d3-gdp-bar-chart'
+  	github: 'https://github.com/jcruzz/d3-gdp-bar-chart',
+    language: 'Javascript',
+    stack: 'D3.js, HTML, CSS',
+    info: `
+            This is a simple bar graph that uses an api request to grab the GDP across the years.
+          `
   },
 ];
