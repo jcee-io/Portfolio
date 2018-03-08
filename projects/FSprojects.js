ba3cb1eaf025a11cb07e80f50dd43adb2257e710
Justin <jvcruz@ucdavis.edu>
2018-03-08 01:54:28 -0800

    Fullstack projects file updated

--- projects/FSprojects.js
+++ projects/FSprojects.js
@@ -3,42 +3,90 @@ module.exports = [
     name: 'Polling.io',
     thumbnail: 'https://i.imgur.com/u1zwc3I.png',
     liveLink: 'http://polling-io.herokuapp.com/',
-    github: 'https://github.com/jcruzz/Polling.io'
+    github: 'https://github.com/jcruzz/Polling.io',
+    language: 'Javascript, SQL',
+    stack: 'MariaDB, React, React-Router, JSONWebToken, Redis, bcrypt, Chart.js, Node/Express',
+    info: `
+          I created this application with the intention for people to create polls in order to enhance their discussion.
+          In order to create polls, you must make an account since polls are bound to a unique user. However, as someone
+          without an account, you're free to vote in polls at your convenience.
+          `
   },
   {
   	name: 'Redux Registration Form',
     thumbnail: null,
     liveLink: 'https://safe-woodland-54646.herokuapp.com/',
-  	github: 'https://github.com/jcruzz/Registration-Form-Redux'
+  	github: 'https://github.com/jcruzz/Registration-Form-Redux',
+    language: 'Javascript',
+    stack: 'PostgreSQL(Bookshelf), React, React-Router, Redux, Node/Express, bcrypt',
+    info: `
+           This is a multi-page application that serves is a sign up sheet that registers accounts. Everytime you click
+           next on a page, information saves into the database as you go through the sign up form. 
+           This was created in a 24 hour challenge that ended up into deployment.
+          `
   },
   {
   	name: 'Saber Forums',
     thumbnail: null,
   	liveLink: 'http://saber-forums.herokuapp.com/#/',
-    github: 'https://github.com/jcruzz/Saber-Forums'
+    github: 'https://github.com/jcruzz/Saber-Forums',
+    language: 'Javascript',
+    stack: 'MongoDB(Mongoose), React, React-Router, Node/Express, Passport',
+    info: `
+           This is a forum created to be viewed by the public, but can only create threads and commented on by
+           registered users. I developed this app with a team in an Agile environment. My role was to be in charge
+           of the authentication suite, and rendering forum threads and thread view.
+          `
   },
   {
   	name: 'WhaleThen',
     thumbnail: null,
   	liveLink: null,
-    github: 'https://github.com/jcruzz/whalethen'
+    github: 'https://github.com/jcruzz/whalethen',
+    language: 'Javascript',
+    stack: 'MongoDB(Mongoose), React, React-Router, Redux, Redux-Thunk, Node/Express, Annyang Speech Module',
+    info: `
+           This is a community-based trip planning application that renders a calendar developed with a team
+           in an Agile environment. My job was to introduce Redux to an existing React codebase in order to 
+           ease future developments in a growing codebase.
+          `
   },
   {
   	name: 'Water Walls',
     thumbnail: null,
   	liveLink: null,
-    github: 'https://github.com/jcruzz/water-walls'
+    github: 'https://github.com/jcruzz/water-walls',
+    language: 'Javascript',
+    stack: 'Node/Express',
+    info: `
+           This is a full stack application that uses the backend to take in an input of integers, access an algorithm,
+           and return an api that contains the results returned by the algorithm. The algorithm solves the problem of having
+           walls of varying height next to each other, and seeing the most water being captured between specific walls.
+          `
   },
   {
   	name: 'Char Sortinator',
     thumbnail: null,
   	liveLink: 'https://damp-falls-31971.herokuapp.com/',
-    github: 'https://github.com/jcruzz/Char-Sortinator-Part-Deux'
+    github: 'https://github.com/jcruzz/Char-Sortinator-Part-Deux',
+    language: 'Javascript',
+    stack: 'Node/Express',
+    info: `
+           This is a small application that takes in an input of a string, uses the server as a sorting microservice,
+           and renders a resulting string with the characters sorted lexicographically.
+          `
   },
   {
   	name: 'PokeRoster',
     thumbnail: null,
   	liveLink: 'https://pokeroster.herokuapp.com/',
-    github: 'https://github.com/jcruzz/PokeRoster'
+    github: 'https://github.com/jcruzz/PokeRoster',
+    language: 'Javascript',
+    stack: 'MongoDB(Mongoose), React, React-Router, Node/Express',
+    info: `
+           This is a roster generator that uses the Pokemon module to list a bunch of pokemon, and uses 
+           react-pokemon-gif to generate visuals on a Pokemon roster being drafted. The roster is then 
+           saved to the database, and is available for viewing.
+          `
   },
 ];
