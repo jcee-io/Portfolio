79084a930f90a6eb337bd5f15b68df7711d41b7e
Justin <jvcruz@ucdavis.edu>
2018-03-08 02:20:34 -0800

    tools project file updated

--- projects/tools.js
+++ projects/tools.js
@@ -1,20 +1,40 @@
 module.exports = [
   {
-    name: 'JWT-Auth React Starter',
+    name: 'JWT-Auth React Boilerplate',
     thumbnail: null,
     liveLink: 'https://frozen-depths-11875.herokuapp.com/',
-    github: 'https://github.com/jcruzz/react-router-auth-boilerplate-sql'
+    github: 'https://github.com/jcruzz/react-router-auth-boilerplate-sql',
+    language: 'Javascript, SQL',
+    stack: 'MariaDB, React, React-Router, JSONWebToken, Redis, bcrypt, Chart.js, Node/Express',
+    info: `
+            This is a boilerplate I've created in order to ease future developments when I develop
+            future projects that require authentication. It's also doubles as a React-Router boilerplate.
+            Future developments will include styling, making a different version with a different database,
+            and include social media authentication in order to hit the ground running when developing
+            a new project. 
+          `
   },
   {
   	name: 'React with Redux Starter',
     thumbnail: null,
   	liveLink: null,
-    github: 'https://github.com/jcruzz/redux-starter'
+    github: 'https://github.com/jcruzz/redux-starter',
+    language: 'Javascript',
+    stack: 'React, Redux, Node/Express',
+    info: `
+            A React boilerplate that comes with Redux.
+          `
+  },
   },
   {
   	name: 'React Starter',
     thumbnail: null,
   	liveLink: null,
-    github: 'https://github.com/jcruzz/react-starter'
+    github: 'https://github.com/jcruzz/react-starter',
+    language: 'Javascript',
+    stack: 'React, Node/Express',
+    info: `
+            A React boilerplate.
+          `
   },
 ];
